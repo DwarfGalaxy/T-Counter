a6647a91a03c473dabe3dba571c00a7d1ddb620f
Yubraj <nontoxicyubs@gmail.com>
2022-05-29 10:01:05 +0545

    Time modified

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ let gethours = document.getElementById("hours");
 let getminutes = document.getElementById("minutes");
 let getseconds = document.getElementById("seconds");
 
-let deadline = new Date("Mar 29,2023 23:59:59").getTime();
+let deadline = new Date("Mar 28,2023 23:59:59").getTime();
 
 
 let showTime = setInterval(() => {
@@ -23,10 +23,10 @@ let showTime = setInterval(() => {
         getseconds.innerText = seconds;
     } else {
         clearInterval(showTime);
-        getdays.innerText = "Happy Birthday love you!";
-        gethours.innerText = "Happy Birthday love you!";
-        getminutes.innerText = "Happy Birthday love you!";
-        getseconds.innerText = "Happy Birthday love you!";
+        getdays.innerText = "Sorry! Time Out";
+        gethours.innerText = "Sorry! Time Out";
+        getminutes.innerText = "Sorry! Time Out";
+        getseconds.innerText = "Sorry! Time Out";
         let playmusic=document.getElementById("playmusic");
         playmusic.innerHTML=`<embed src="/audio/hbd.mp3" autostart="true" width="2" height="0" type="audio/mpeg">`
     }
